537ef9afaf34bd6417dc9af0168e45dd9cd2fb9b
Shreya-0007 <ss0684397@gmail.com>
2026-02-28 23:27:45 +0530

    Print system initialization message

--- src/main.cpp
+++ src/main.cpp
@@ -25,3 +25,8 @@ void setup() {
     // TODO 3:
     // Initialize Serial communication (9600 baud rate)
     Serial.begin(9600);
+
+    // TODO 4:
+    // Print system initialization message
+    Serial.println("LDR Light Detection System Initialized...");
+}
